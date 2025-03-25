1c2f7f18cc825c1d875cde316cf96df6b93c14a4
Ilay cohen <77060249+ilayc11@users.noreply.github.com>
2025-03-25 10:35:50 +0000

    added logging

--- ResultsAnalysis.sh
+++ ResultsAnalysis.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+logToLog(){
+timestamp=$(date +"%d/%m/%Y %H:%M:%S")
+echo "$timestamp [$1] $2 $3" >> "$log_file"
+}
+
 SlowestAndFastest() {
+	logToLog "INFO" "ResultsAnalysis.sh" "Finding_Slowest_And_Fastest"
 	fastestTime=9999999
 	fastestSite=""
 	slowestTime=0
@@ -21,17 +27,22 @@ SlowestAndFastest() {
 			fi
 		done
 	done < "$input_file"
+	logToLog "INFO" "ResultsAnalysis.sh" "found_Slowest_And_Fastest"
 	echo "Slowest: $slowestSite $slowestTime" >> ResultsAnalysis.txt
 	echo "Fastest: $fastestSite $fastestTime" >> ResultsAnalysis.txt
+	logToLog "INFO" "ResultsAnalysis.sh" "wrote_slowest_and_fastest_to_$input_file"
 }
 
 RankByAvg(){
+	logToLog "INFO" "ResultsAnalysis.sh" "starting_Rank_By_Avg"
 	echo "Ranking:" >> ResultsAnalysis.txt
 	sort -k7 -n "$input_file" | awk '{print $1}' >> ResultsAnalysis.txt
+	logToLog "INFO" "ResultsAnalysis.sh" "wrote_ranking_to_$input_file"
 }
 
 
 calculateAvgOfAvg(){
+	logToLog "INFO" "ResultsAnalysis.sh" "starting_avg_of_avg_calculation"
 	avgSum=0
 	numberOfSites=0
 	while read -r site t1 t2 t3 t4 t5 avgT; do 
@@ -42,12 +53,30 @@ calculateAvgOfAvg(){
 		(( numberOfSites = numberOfSites + 1 ))
 	done < "$input_file"
 	echo "Overall avg latency: $(echo "scale=3; $avgSum / $numberOfSites" | bc -l)" >> ResultsAnalysis.txt
+	logToLog "INFO" "ResultsAnalysis.sh" "wrote_avg_of_avg_to_$input_file"
 }
 
+
+logToLog "INFO" "ResultsAnalysis.sh" "started_script"
+
 input_file="PingResults.txt"
+log_file="pingstorm.log"
+
+if [[ ! -e "$input_file" ]]; then
+	echo "input file doesnt exist!"
+	logToLog "ERROR" "ResultsAnalysis.sh" "input_file_doesnt_exist"
+	exit
+elif [[ -z "$input_file" ]]; then
+	echo "input file doesnt include sites!"
+	logToLog "ERROR" "ResultsAnalysis.sh" "input_file_is_empty"
+	exit
+fi
+
 SlowestAndFastest
 RankByAvg
 calculateAvgOfAvg
+
+logToLog "INFO" "ResultsAnalysis.sh" "finished_script"
 
 
 
