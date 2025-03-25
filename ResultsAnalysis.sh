372e9646d77f8c0b31a1c98fe89a2a8381571aac
Ilay cohen <77060249+ilayc11@users.noreply.github.com>
2025-03-25 09:41:10 +0000

    tabs

--- ResultsAnalysis.sh
+++ ResultsAnalysis.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 SlowestAndFastest() {
-fastestTime=9999999
-fastestSite=""
-slowestTime=0
-slowestSite=""
-while read -r site t1 t2 t3 t4 t5 avgT; do
-if [[ -z "$site" ]]; then
-	continue
-fi
-	for time in $t1 $t2 $t3 $t4 $t5; do
-		#fastest
-		if (( $(echo "$time < $fastestTime" | bc -l) )); then
-			fastestTime=$time
-			fastestSite=$site
-		fi
-		#slowest
-		if (( $(echo "$time > $slowestTime" | bc -l) )); then
-			slowestTime=$time
-			slowestSite=$site
-		fi
-	done
-done < "$input_file"
-echo "fastest time=$fastestTime fastest site=$fastestSite slowest time=$slowestTime slowest site=$slowestSite"
+	fastestTime=9999999
+	fastestSite=""
+	slowestTime=0
+	slowestSite=""
+	while read -r site t1 t2 t3 t4 t5 avgT; do 
+	if [[ -z "$site" ]]; then
+		continue
+	fi
+		for time in $t1 $t2 $t3 $t4 $t5; do
+			#fastest
+			if (( $(echo "$time < $fastestTime" | bc -l) )); then
+				fastestTime=$time
+				fastestSite=$site
+			fi
+			#slowest
+			if (( $(echo "$time > $slowestTime" | bc -l) )); then
+				slowestTime=$time
+				slowestSite=$site
+			fi
+		done
+	done < "$input_file"
+	echo "fastest time=$fastestTime fastest site=$fastestSite slowest time=$slowestTime slowest site=$slowestSite"
 }
 
 #RankByAvg(){
